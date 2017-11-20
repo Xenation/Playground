7f5fa58e456d01cab95d9e4a9242a0b4965c9d05
Xenation <xenations.111@gmail.com>
2017-11-20 16:53:00 +0100

    Folders Cleaning and Stencil portals WIP

---
+++ Assets/Projects/Portals/Scripts/MainPortalsCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Playground.Portals {
+	public class MainPortalsCamera : MonoBehaviour {
+
+		public Material portalRedPost;
+		public Material portalBluePost;
+
+		public void OnRenderImage(RenderTexture source, RenderTexture destination) {
+			RenderTexture tmp = RenderTexture.GetTemporary(source.width, source.height, 24);
+			Graphics.Blit(source, tmp, portalRedPost);
+			Graphics.Blit(tmp, destination, portalBluePost);
+		}
+
+	}
+}
---
+++ Assets/Projects/Portals/Scripts/PortalCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Playground.Portals {
+	public class PortalCamera : MonoBehaviour {
+
+		public Material postprocess;
+
+		public void OnRenderImage(RenderTexture source, RenderTexture destination) {
+			Graphics.Blit(source, destination, postprocess);
+		}
+
+	}
+}
