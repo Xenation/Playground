e437900e427b95262ed678505d6139730aceda62
Xenation <xenations.111@gmail.com>
2017-11-22 16:56:19 +0100

    Editor Window Tests

---
+++ Assets/Projects/Editor/Scripts/EditorTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Playground.Editor {
+	public class EditorTest : EditorWindow {
+
+		private SerializedObject winObj;
+		public GameObject editDebug;
+		private SerializedProperty propEditDebug;
+
+		[MenuItem("Window/EditorTest")]
+		public static void ShowWindow() {
+			GetWindow<EditorTest>();
+		}
+
+		public void OnEnable() {
+			// Value init
+			editDebug = null;
+			// Properties init
+			winObj = new SerializedObject(this);
+			propEditDebug = winObj.FindProperty("editDebug");
+		}
+
+		public void OnGUI() {
+			EditorGUILayout.PropertyField(propEditDebug, new GUIContent("edit debug"));
+			if (GUILayout.Button("Test")) {
+				Undo.RecordObject(editDebug.transform, "1 up");
+				editDebug.transform.position += Vector3.up;
+			}
+			winObj.ApplyModifiedProperties();
+		}
+
+	}
+}
