2df28b7e5361928fb3dfbd2abd84032d151a8bad
Xenation <xenations.111@gmail.com>
2018-01-13 22:57:37 +0100

    Xenon Tools Base with Wireframe switch and Deselector

---
+++ Assets/Projects/Editor/Scripts/Essentials.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Playground {
+	public class Essentials : EditorWindow {
+
+		private const float WIN_MIN_WIDTH = 300f;
+		private const float WIN_MIN_HEIGHT = 200f;
+
+		private bool shortcutsEnabled = true;
+
+		private bool ctrl = false;
+
+		public static void ShowWindow() {
+			Essentials win = GetWindow<Essentials>("Essentials");
+			win.minSize = new Vector2(WIN_MIN_WIDTH, WIN_MIN_HEIGHT);
+		}
+
+		public void ResetModifiers() {
+			ctrl = false;
+		}
+
+		private void Awake() {
+			SceneView.onSceneGUIDelegate -= OnSceneGUI;
+			SceneView.onSceneGUIDelegate += OnSceneGUI;
+		}
+
+		private void OnDestroy() {
+			SceneView.onSceneGUIDelegate -= OnSceneGUI;
+		}
+
+		private void OnGUI() {
+			shortcutsEnabled = EditorGUILayout.ToggleLeft("Shortcuts Enabled", shortcutsEnabled);
+			if (!shortcutsEnabled) {
+				ResetModifiers();
+			}
+		}
+
+		private void OnSceneGUI(SceneView scene) {
+			Event e = Event.current;
+			if (!shortcutsEnabled && (e.type == EventType.keyDown || e.type == EventType.KeyUp)) {
+				return;
+			}
+			switch (e.type) {
+				case EventType.KeyDown: // DOWN
+					//Debug.Log(e.keyCode); // Pressed debug
+					switch (e.keyCode) {
+						case KeyCode.LeftControl: // Left Control Modifier
+							ctrl = true;
+							break;
+						case KeyCode.W: // Wireframe switch
+							if (ctrl) {
+								SwitchRenderMode(scene);
+							}
+							break;
+						case KeyCode.Backslash: // Deselector
+							DeselectAll();
+							break;
+					}
+					break;
+
+				case EventType.KeyUp: // UP
+					switch (e.keyCode) {
+						case KeyCode.LeftControl: // Left Control Modifier
+							ctrl = false;
+							break;
+					}
+					break;
+			}
+		}
+
+		private void SwitchRenderMode(SceneView sceneView) {
+			switch (sceneView.renderMode) {
+				default:
+				case DrawCameraMode.Textured:
+					sceneView.renderMode = DrawCameraMode.TexturedWire;
+					break;
+				case DrawCameraMode.TexturedWire:
+					sceneView.renderMode = DrawCameraMode.Textured;
+					break;
+			}
+			sceneView.Repaint();
+		}
+
+		private void DeselectAll() {
+			Selection.activeGameObject = null;
+		}
+
+	}
+}
---
+++ Assets/Projects/Editor/Scripts/ScreenshotTool.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Playground {
+	public class ScreenshotTool : EditorWindow {
+
+		private const float WIN_MIN_WIDTH = 300f;
+		private const float WIN_MIN_HEIGHT = 65f;
+
+		private const int SUPER_SIZE_MIN = 1;
+		private const int SUPER_SIZE_MAX = 8;
+
+		private string fileName = "screenshot";
+		private string filePath = "screenshot.png";
+
+		private int superSize = 1;
+
+		public static void ShowWindow() {
+			ScreenshotTool win = GetWindow<ScreenshotTool>("Screenshot Tool");
+			win.minSize = new Vector2(WIN_MIN_WIDTH, WIN_MIN_HEIGHT);
+		}
+
+		private void OnGUI() {
+			fileName = EditorGUILayout.TextField("File", fileName);
+			filePath = fileName + ".png";
+			
+			superSize = EditorGUILayout.IntSlider("Super Size", superSize, SUPER_SIZE_MIN, SUPER_SIZE_MAX);
+
+			if (GUILayout.Button("Save Screenshot")) {
+				ScreenCapture.CaptureScreenshot(filePath, superSize);
+				EditorUtility.RevealInFinder(filePath);
+			}
+		}
+
+	}
+}
